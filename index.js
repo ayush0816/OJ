e403fd36fcf2d8e154a8bd1a5c953f230281d8c8
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-09-28 01:34:14 +0530

    App running

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let num = 2;
+const express = require("express");
+const db = require("./db.js");
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Kudos , The app is running on port", port);
+});
