2ba7fa125cd4d7dee4af944fd2818aa2430f8b17
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-09-28 01:38:48 +0530

    Setting up routes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const db = require("./db.js");
+const testCases=require('./routes/testCase.js');
+const submit=require('./routes/submit.js');
+const result=require('./routes/result.js')
 
 const app = express();
 const port = 3000;
+
+
+app.use('/testCase', testCases);
+app.use('/submit', submit);
+app.use('/result', result);
+
 
 app.listen(port, () => {
   console.log("Kudos , The app is running on port", port);
